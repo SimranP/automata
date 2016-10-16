d7226a877955f6325b6a64c45235e8593f0bfc4d
Simran pal <simranp@thoughtworks.com>
2016-10-16 16:29:51 +0530

    parser is now parsing tuple and delta

--- deterministic_finite_automata/src/com/company/Parser.java
+++ deterministic_finite_automata/src/com/company/Parser.java
@@ -8,15 +8,15 @@ import java.util.*;
 
 
 public class Parser {
-    private JSONObject jsonTuple;
-
-    public void parse(String json) throws ParseException {
+    public ArrayList<JSONObject> parse(String json) throws ParseException {
+        ArrayList<JSONObject> allObjects = new ArrayList<>();
         JSONParser jsonParser = new JSONParser();
-        JSONObject jsonObject = (JSONObject) jsonParser.parse(json);
-        jsonTuple = (JSONObject) jsonObject.get("tuple");
+        JSONArray jsonObjects = (JSONArray) jsonParser.parse(json);
+        for (Object jsonObject: jsonObjects) allObjects.add((JSONObject) jsonObject);
+        return allObjects;
     }
 
-    public HashMap<String, HashSet<String>> tuple(){
+    public HashMap<String, HashSet<String>> tuple(JSONObject jsonTuple){
         HashMap<String, HashSet<String>> tuple = new HashMap<>();
         JSONArray start_state = new JSONArray();
         start_state.add(jsonTuple.get("start-state"));
@@ -32,13 +32,11 @@ public class Parser {
 
     private HashSet<String> convertToHashSet(JSONArray array){
         HashSet<String> hashset = new HashSet<>();
-        for (Object string : array) {
-            hashset.add((String) string);
-        }
+        for (Object string : array) hashset.add((String) string);
         return hashset;
     }
 
-    public Delta delta(){
+    public Delta delta(JSONObject jsonTuple){
         HashSet<String[]> transitions = new HashSet<>();
         JSONObject delta =(JSONObject) jsonTuple.get("delta");
         for (Object key: delta.keySet()) {
@@ -50,5 +48,4 @@ public class Parser {
         }
         return Delta.create(transitions);
     };
-
 }
--- deterministic_finite_automata/test/com/company/ParserTest.java
+++ deterministic_finite_automata/test/com/company/ParserTest.java
@@ -1,15 +1,21 @@
 package com.company;
 
+import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class ParserTest {
     @Test
     public void parse_should_parse_the_JSON_string() throws ParseException {
         Parser parser = new Parser();
-        String a = "{\"name\":\"odd number of zeroes\",\"type\":\"dfa\",\"tuple\":{\"states\":[\"q1\",\"q2\"],\"alphabets\":[\"1\",\"0\"],\"delta\":{\"q1\":{\"0\":\"q2\",\"1\":\"q1\"},\"q2\":{\"0\":\"q1\",\"1\":\"q2\"}},\"start-state\":\"q1\",\"final-states\":[\"q2\"]},\"pass-cases\":[\"0\",\"000\",\"00000\",\"10\",\"101010\",\"010101\"],\"fail-cases\":[\"00\",\"0000\",\"1001\",\"1010\",\"001100\"]}";
-        parser.parse(a);
-        parser.tuple();
-        parser.delta();
+        String jsonString = "[{\"should_run_pass_cases_and_fail_cases_for_machine_of_each_json_object\":\"odd number of zeroes\",\"type\":\"dfa\",\"tuple\":{\"states\":[\"q1\",\"q2\"],\"alphabets\":[\"1\",\"0\"],\"delta\":{\"q1\":{\"0\":\"q2\",\"1\":\"q1\"},\"q2\":{\"0\":\"q1\",\"1\":\"q2\"}},\"start-state\":\"q1\",\"final-states\":[\"q2\"]},\"pass-cases\":[\"0\",\"000\",\"00000\",\"10\",\"101010\",\"010101\"],\"fail-cases\":[\"00\",\"0000\",\"1001\",\"1010\",\"001100\"]}]";
+        ArrayList<JSONObject> parsedJSON = parser.parse(jsonString);
+        for (JSONObject object:parsedJSON) {
+            Object tuple = object.get("tuple");
+            parser.tuple((JSONObject) tuple);
+            parser.delta((JSONObject) tuple);
+        }
     }
 }
